55f33f8ad7ce7348e8b29fae07c085b388f85343
markkhalil <mark3essam@gmail.com>
2023-06-06 16:04:35 +0300

    e1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <img src='https://miro.medium.com/v2/resize:fit:1358/0*EitUXT-pqbaQSCTt.gif'/>
+            <h1>Edit src/App.js and save to reload.</h1>
+            <a>Learn React</a>
+        </div>
+    )
+};
+
+export default App;
